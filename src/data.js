3c289b5584d65e31c97a5ea4fdf07ec2157868cd
Waldo Smuts <wb.smuts@gmail.com>
2022-03-29 20:10:50 +0200

    added seed data

---
+++ src/data.js
@@ -0,0 +1,93 @@
+const data = {
+    products: [
+        {
+            title: "Mixed Vegetables",
+            prices: [
+                {
+                    weight: 50,
+                    price: 58
+                },
+                {
+                    weight: 100,
+                    price: 112
+                },
+            ],
+            preview: "",
+            description: "This mixed vegetable pack is made with locally grown, organic vegetables. Its the perfect addition to your Asian inspired noodle dish, pesto pasta, or a side for your braaied dinner.",
+            ingredients: ["onion", "carrot", "pepper", "zucchini"],
+            shelfLife: 1,
+        },
+        {
+            title: "Carrot",
+            categories: ["veg"],
+            prices: [
+                {
+                    weight: 20,
+                    price: 18
+                },
+                {
+                    weight: 40,
+                    price: 36
+                },
+            ],
+            preview: "",
+            description: "Our dehydrated carrots are made with locally grown, organic carrots. It's the perfect addition to your Asian inspired noodle dish, pesto pasta, or a side for your braaied dinner.",
+            ingredients: ["carrot"],
+            shelfLife: 1,
+        },
+        {
+            title: "Zucchini",
+            categories: ["veg"],
+            prices: [
+                {
+                    weight: 20,
+                    price: 42
+                },
+                {
+                    weight: 40,
+                    price: 80
+                },
+            ],
+            preview: "",
+            description: "Our dehydrated baby marrows are made with locally grown, organic courgettes (a few names for the zucchinis!). It's the perfect addition to your Asian inspired noodle dish, pesto pasta, or a side for your braaied dinner.",
+            ingredients: ["zucchini"],
+            shelfLife: 1,
+        },
+        {
+            title: "Onion",
+            categories: ["veg"],
+            prices: [
+                {
+                    weight: 20,
+                    price: 18
+                },
+                {
+                    weight: 40,
+                    price: 36
+                },
+            ],
+            preview: "",
+            description: "Our dehydrated onions are made with locally grown, organic onions. It's the perfect addition to your Asian inspired noodle dish, pesto pasta, or a side for your braaied dinner.",
+            ingredients: ["onion"],
+            shelfLife: 1,
+        },
+        {
+            title: "Red Pepper",
+            categories: ["veg"],
+            prices: [
+                {
+                    weight: 20,
+                    price: 42
+                },
+                {
+                    weight: 40,
+                    price: 80
+                },
+            ],
+            preview: "",
+            description: "Our dehydrated red peppers are made with locally grown, organic red peppers. It's the perfect addition to your Asian inspired noodle dish, pesto pasta, or a side for your braaied dinner.",
+            ingredients: ["pepper"],
+            shelfLife: 1,
+        },
+    ]
+}
